3f3547796c33577874666e09bba754c11c3b113f
鈴木啓太 <167952725+Keita062@users.noreply.github.com>
2026-03-21 18:45:45 +0900

    Auth (Cognito) の構築とS3の構築の完了

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "basebanksystem7767fdaf": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3a577ddde": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
