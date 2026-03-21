549e2aa5f87a7525010917175c16bc007d4335cc
鈴木啓太 <167952725+Keita062@users.noreply.github.com>
2026-03-21 22:00:15 +0900

    Add GraphQL file

---
+++ demo/js/graphql/queries.js
@@ -0,0 +1,191 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getDeal = /* GraphQL */ `
+  query GetDeal($id: ID!) {
+    getDeal(id: $id) {
+      id
+      staff_name
+      venue_id
+      venueLabel
+      customer
+      status
+      total_buy_price
+      hqAnswer
+      items {
+        nextToken
+        __typename
+      }
+      messages {
+        nextToken
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listDeals = /* GraphQL */ `
+  query ListDeals(
+    $filter: ModelDealFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDeals(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        staff_name
+        venue_id
+        venueLabel
+        customer
+        status
+        total_buy_price
+        hqAnswer
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getItem = /* GraphQL */ `
+  query GetItem($id: ID!) {
+    getItem(id: $id) {
+      id
+      dealID
+      category
+      itemName
+      weight
+      buy_price
+      condition
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listItems = /* GraphQL */ `
+  query ListItems(
+    $filter: ModelItemFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        dealID
+        category
+        itemName
+        weight
+        buy_price
+        condition
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getMessage = /* GraphQL */ `
+  query GetMessage($id: ID!) {
+    getMessage(id: $id) {
+      id
+      dealID
+      sender_role
+      text
+      sent_at
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listMessages = /* GraphQL */ `
+  query ListMessages(
+    $filter: ModelMessageFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listMessages(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        dealID
+        sender_role
+        text
+        sent_at
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const itemsByDealID = /* GraphQL */ `
+  query ItemsByDealID(
+    $dealID: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelItemFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    itemsByDealID(
+      dealID: $dealID
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        dealID
+        category
+        itemName
+        weight
+        buy_price
+        condition
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const messagesByDealID = /* GraphQL */ `
+  query MessagesByDealID(
+    $dealID: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelMessageFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    messagesByDealID(
+      dealID: $dealID
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        dealID
+        sender_role
+        text
+        sent_at
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
